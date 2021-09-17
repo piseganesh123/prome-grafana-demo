fdebd17f74af4fa58c48e7667f3c92ae27074dfa
Ganesh Pise <piseganesh123@gmail.com>
2021-09-17 13:27:44 +0530

    updated config

--- grafana-config.sh
+++ grafana-config.sh
@@ -4,8 +4,7 @@
 sudo apt-get install -y apt-transport-https
 sudo apt-get install -y software-properties-common wget
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/gra
-fana.list
+echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt-get install grafana-enterprise
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl start grafana-server
