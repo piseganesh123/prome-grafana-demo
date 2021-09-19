b5b794ebb361d87369b21ad6cfe095c7aa83adea
piseganesh123 <piseganesh123@gmail.com>
2021-09-19 15:43:38 +0530

    Update prom-grafana-config.tf

--- prom-grafana-config.tf
+++ prom-grafana-config.tf
@@ -8,7 +8,7 @@ variable "project_id" {
 
 provider "google" {
 // credentials = file("/home/piseg432/keys/gce-creator.json")
- credentials =  $(GOOGLE_CLOUD_KEYFILE_JSON)
+// credentials =  $(GOOGLE_CLOUD_KEYFILE_JSON)
  //project     = "vast-pad-319812"
  project     = var.project_id
  region      = "asia-south1"
